d08c72723539ad5ee2d2adefa2e51d69be240ce2
Abeeb Alimi <68944389+Echo-te@users.noreply.github.com>
2024-09-21 08:37:23 +0100

    Delete oig_contributions directory

--- oig_contributions/models/region.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class OIGRegion(models.Model):
-    _name = 'oig.region'
-    _description = 'OIG Region'
-
-    name = fields.Char(string='Region Name', required=True)
